684b4cc57821781077021d7482feb56f79230ce8
Zhanerrrkeee <124174188+Zhanerrrkeee@users.noreply.github.com>
2023-02-12 22:32:13 +0600

    Create data2.py

---
+++ lab4/data2.py
@@ -0,0 +1,15 @@
+from datetime import datetime,timedelta
+
+date_format='%d.%m.%Y'
+
+x=datetime.now()
+
+yesterday=x - timedelta(days=1)
+
+today=x
+
+tomorrow=x + timedelta(days=1)
+
+print( yesterday.strftime(date_format))
+print(today.strftime(date_format))
+print(tomorrow.strftime(date_format))
